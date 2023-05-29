4a4d7dc043b89ff9358aaa91a382e83a8c8940dd
Arvind <thakurarvindkr10@gmail.com>
2023-05-29 18:13:23 +0530

    login and navbar done and logOut functionality also done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './Navbar'
 import Products from './Products'
+import { auth, fs } from '../Config/Config'
 
 const Home = () => {
+    // get the current user
+    function GetCurrentUser(){
+        const [user, setUser]=useState(null);
+        useEffect(()=>{
+            auth.onAuthStateChanged(user=>{
+                if(user){
+                    fs.collection('users').doc(user.uid).get().then(snapshot=>{
+                        setUser(snapshot.data().FullName);
+                    })
+                }
+                else{
+                    setUser(null);
+                }
+            })
+        },[])
+        return user;
+    }
+    const user = GetCurrentUser();
+    console.log(user)
     return (
         <div>
-            <Navbar />
+            <Navbar user={user} />
             <Products />
         </div>
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,44 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import logo from '../img/cart-icon-28341(1).png'
+import { HiOutlineShoppingCart } from 'react-icons/hi'
+import { auth } from '../Config/Config'
 
-const Navbar = () => {
+
+const Navbar = ({ user }) => {
+    const navigate = useNavigate();
+    const handleLogOut = () => {
+        auth.signOut().then(() => {
+            navigate("/login")
+        })
+    }
     return (
-        <div>
-            <Link to='login'>LogIn</Link>
-            <Link to='signUp'>SignUp</Link>
+        <div className='navbar'>
+            <div className='leftside'>
+                <div className='logo'>
+                    <img src={logo} alt="logo" />
+                </div>
+            </div>
+            <div className='rightside'>
+                {!user &&
+                    <>
+                        <div><Link className='navlink' to="signup">SIGN UP</Link></div>
+                        <div><Link className='navlink' to="login">LOGIN</Link></div>
+                    </>}
+
+                {user && <>
+                    <div><Link className='navlink' to='/'>{user}</Link></div>
+                    <div className="cart-menu-btn">
+                        <Link className="navlink" to='/cart'>
+                            <HiOutlineShoppingCart size={'35px'} />
+                        </Link>
+                        {/* <span className='cart-indicator'>{totalQty}</span> */}
+                    </div>
+                    <div className="btn btn-danger btn-md" onClick={handleLogOut}>
+                        LOGOUT
+                    </div>
+                </>}
+            </div>
         </div>
     )
 }
