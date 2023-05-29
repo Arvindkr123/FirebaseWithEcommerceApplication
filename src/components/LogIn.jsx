88bc2f4440f35229dfa46d26e6a219ebb132f77a
Arvind <thakurarvindkr10@gmail.com>
2023-05-29 15:32:21 +0530

    login done

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,15 +1,27 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { auth } from '../Config/Config';
 
 const LogIn = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [errorMsg, setErrorMsg] = useState('');
     const [successMsg, setSuccessMsg] = useState('');
+    const naviagate = useNavigate();
 
     const handleLogin = (e) => {
         e.preventDefault();
-        console.log(email, password)
+        console.log(email, password);
+        auth.signInWithEmailAndPassword(email, password).then(() => {
+            setSuccessMsg('Login successfull, you will now automatically get redirected to Home...')
+            setEmail('');
+            setPassword('')
+            setErrorMsg('')
+            setTimeout(() => {
+                setSuccessMsg('');
+                naviagate("/")
+            }, 3000)
+        }).catch(error => setErrorMsg(error.message))
     }
     return (
         <div className='container'>
@@ -17,6 +29,10 @@ const LogIn = () => {
             <br /><br />
             <h1>LogIn</h1>
             <hr /><hr />
+            {successMsg && <>
+                <div className='success-msg'>{successMsg}</div>
+                <br></br>
+            </>}
             <form className='form-group' autoComplete='off' onSubmit={handleLogin}>
                 <label>Email</label>
                 <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" className='form-control' required />
@@ -28,6 +44,10 @@ const LogIn = () => {
                     <button type='submit' className='btn btn-success btn-md text-uppercase'>LogIn</button>
                 </div>
             </form>
+            {errorMsg && <>
+                <br></br>
+                <div className='error-msg'>{errorMsg}</div>
+            </>}
         </div>
     )
 }
